0df1da94c33597a7c6b997180287c34b73ba1be8
Ибрагим Габидуллин <ibragim0795@mail.ru>
2018-04-10 21:06:45 +0300

    Added client project

---
+++ AIProject/Client.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AIProject
+{
+
+    public class Client
+    {
+        public Client(string ip, int port)
+        {
+            this.ip = ip;
+            this.port = port;
+        }
+        private NetworkStream ns;
+        private string path = string.Empty;
+        private string ip;
+        private int port;
+
+        public void StartUploading(String S)
+        {
+            path = S;
+        }
+
+        public void Run()
+        {
+            Thread mainThread = new Thread(() =>
+            {
+                TcpClient _client = new TcpClient("127.0.0.1", 808);
+                ns = _client.GetStream();
+
+                while (true)
+                {
+                    if (path.Length > 0 && path != string.Empty && ns.CanWrite)
+                    {
+                        byte[] file = File.ReadAllBytes(path);
+                        byte[] fileBuffer = new byte[file.Length];
+                        ns.Write(file.ToArray(), 0, fileBuffer.GetLength(0));
+
+                        path = string.Empty;
+
+                        ns.Flush();
+                    }
+                }
+            });
+            mainThread.IsBackground = true;
+            mainThread.Start();
+        }
+    }
+}
---
+++ AIProject/Client.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AIProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Client client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            client = new Client("127.0.0.1", 808);
+            client.Run();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            client.Run();
+            client.StartUploading("Start");
+        }
+    }
+}
